43cf7559b12a6606c56ed09ced4ffe677cba1373
Faizan <faizanali3n@gmail.com>
2025-09-06 17:00:38 +0500

    Updated

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -11,7 +11,16 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: ()=>{},
-        removeTodo: ()=>{},
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+
+            }
+        state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload.id);
+        },
     }
-    })
+});
